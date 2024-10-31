b2c3470a6cc7813e30b821a4bb800d2da30bc160
ClaudiaDA <claudiaiad@gmail.com>
2024-10-31 21:03:18 +0000

    primero

--- models.py
+++ models.py
@@ -2,10 +2,10 @@ from pydantic import BaseModel, Field, EmailStr
 from typing import List, Optional 
      
 class Usuario(BaseModel):
-    id: str = Field(..., example='p1')
-    nombre: str= Field(..., example='Juan')
-    email: str = Field(..., example='juan.judas@example.com')
-    edad: int = Field(..., example='2024-10-23T19:00:00Z')
+    id: str 
+    nombre: str
+    email: str 
+    edad: int 
 
 class Proyecto(BaseModel):
     id: str 
